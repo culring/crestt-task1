08ca2e94c12a5524c6c7d6dacf6af63789963dc9
Rafał Wójcik <rafalwojcik@e-point.pl>
2020-07-21 22:54:57 +0200

    Refactor

--- src/main/scala/xlsx2node/Xlsx2NodeParser.scala
+++ src/main/scala/xlsx2node/Xlsx2NodeParser.scala
@@ -7,6 +7,9 @@ import org.apache.poi.ss.usermodel.{DataFormatter, WorkbookFactory}
 import scala.jdk.CollectionConverters._
 
 object Xlsx2NodeParser {
+  val NUMBER_OF_CELLS_IN_ROW = 4;
+  val NUMBER_OF_LEVELS = 3;
+
   def parse(inputStream: InputStream): List[Node] = {
     val rows = readXlsx(inputStream)
     val parsedRows = parseToRows(rows)
@@ -17,38 +20,39 @@ object Xlsx2NodeParser {
     val workbook = WorkbookFactory.create(inputStream)
     val sheet = workbook.getSheetAt(0)
     val formatter = new DataFormatter()
-    val output = for(row <- sheet.asScala.drop(1)) yield {
-      for(i <- List.range(0, 4)) yield {
+    val rows = for(row <- sheet.asScala.drop(1)) yield {
+      for(i <- List.range(0, NUMBER_OF_CELLS_IN_ROW)) yield {
         val cell = row.getCell(i, org.apache.poi.ss.usermodel.Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)
         if(cell != null) Some(formatter.formatCellValue(cell))
         else None
       }
     }
     inputStream.close()
-    output.toList
+    rows.toList
   }
 
   def parseToRows(rows: List[List[Option[String]]]): List[Row] = {
     for(row <- rows) yield {
       val Id = row(3).get.toInt
       val level = extractLevelFromRow(row)
-      val name = row(level).get
+      val name = row(level-1).get
       Row(Id, level, name)
     }
   }
 
   private def extractLevelFromRow(row: List[Option[String]]): Int = {
-    row.indexWhere(cell =>
+    val index = row.indexWhere(cell =>
       cell match {
         case Some(_) => true
         case None => false
       }
     )
+    index+1
   }
 
   def parseToNodes(rows: List[Row]): List[Node] = {
     def getChildren(siblingsLevel: Int, rows: List[Row], siblings: List[Node]): (List[Node], List[Row]) = {
-      if(rows.isEmpty || siblingsLevel > 2) {
+      if(rows.isEmpty || siblingsLevel > NUMBER_OF_LEVELS) {
         return (siblings, rows)
       }
       val currentRow = rows.head
@@ -61,7 +65,7 @@ object Xlsx2NodeParser {
       getChildren(siblingsLevel, rowsLeft, siblings :+ currentNode)
     }
 
-    val (nodes, _) = getChildren(0, rows, List())
+    val (nodes, _) = getChildren(1, rows, List())
     nodes
   }
 }
